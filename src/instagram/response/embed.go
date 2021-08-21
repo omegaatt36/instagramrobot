67245be804069a9a3dc42c0cce8b57f46cbf605e
Hooman Hassanzadeh <mr.subscript@gmail.com>
2021-08-21 17:54:55 +0430

    refactor(instagram): embed response

--- src/instagram/response/embed.go
+++ src/instagram/response/embed.go
@@ -7,10 +7,10 @@ type Dimensions struct {
 	Width  int `json:"width"`
 }
 
-type DisplayResource struct {
-	ConfigWidth  int    `json:"config_width"`
-	ConfigHeight int    `json:"config_height"`
-	Src          string `json:"src"`
+type Resource struct {
+	Width  int    `json:"config_width"`
+	Height int    `json:"config_height"`
+	Src    string `json:"src"`
 }
 
 type Caption struct {
@@ -25,51 +25,63 @@ type WithCount struct {
 	Count uint64 `json:"count"`
 }
 
+type OwnerTimeline struct {
+	Count uint64 `json:"count"`
+	Edges []struct {
+		Node struct {
+			Id                 string     `json:"string"`
+			ThumbnailSrc       string     `json:"thumbnail_src"`
+			ThumbnailResources []Resource `json:"thumbnail_resources"`
+		} `json:"node"`
+	} `json:"edges"`
+}
+
 type Owner struct {
-	Id                string    `json:"id"`
-	ProfilePictureURL string    `json:"profile_pic_url"`
-	Username          string    `json:"username"`
-	HasPublicStory    bool      `json:"has_public_story"`
-	IsPrivate         bool      `json:"is_private"`
-	IsVerified        bool      `json:"is_verified"`
-	FollowedBy        WithCount `json:"edge_followed_by"`
-	// TODO: add more fields
+	Id                string        `json:"id"`
+	ProfilePictureURL string        `json:"profile_pic_url"`
+	Username          string        `json:"username"`
+	HasPublicStory    bool          `json:"has_public_story"`
+	IsPrivate         bool          `json:"is_private"`
+	IsVerified        bool          `json:"is_verified"`
+	FollowedBy        WithCount     `json:"edge_followed_by"`
+	Timeline          OwnerTimeline `json:"edge_owner_to_timeline_media"`
+}
+
+type ShortcodeMedia struct {
+	Type             string     `json:"__typename"`
+	Id               string     `json:"id"`
+	Shortcode        string     `json:"shortcode"`
+	ProductType      string     `json:"product_type"`
+	TakenAt          types.Time `json:"taken_at_timestamp"`
+	CommenterCount   uint64     `json:"commenter_count"`
+	Comments         WithCount  `json:"edge_media_to_comment"`
+	Likes            WithCount  `json:"edge_liked_by"`
+	Dimensions       Dimensions `json:"dimensions"`
+	DisplayURL       string     `json:"display_url"`
+	DisplayResources []Resource `json:"display_resources"`
+
+	Caption Caption `json:"edge_media_to_caption"`
+	Owner   Owner   `json:"owner"`
+
+	IsVideo        bool   `json:"is_video"`
+	Title          string `json:"title"`
+	VideoURL       string `json:"video_url"`
+	VideoViewCount uint64 `json:"video_view_count"`
+
+	// clips_music_attribution_info
+	// media_overlay_info
+	// sharing_friction_info
+	// edge_media_to_sponsor_user
+	// is_affiliate
+	// is_paid_partnership
+	// location
+	// coauthor_producers
 }
 
 type EmbedResponse struct {
-	ShortcodeMedia struct {
-		Type             string            `json:"__typename"`
-		Id               string            `json:"id"`
-		Shortcode        string            `json:"shortcode"`
-		ProductType      string            `json:"product_type"`
-		TakenAt          types.Time        `json:"taken_at_timestamp"`
-		CommenterCount   uint64            `json:"commenter_count"`
-		Comments         WithCount         `json:"edge_media_to_comment"`
-		Likes            WithCount         `json:"edge_liked_by"`
-		Dimensions       Dimensions        `json:"dimensions"`
-		DisplayURL       string            `json:"display_url"`
-		DisplayResources []DisplayResource `json:"display_resources"`
-
-		Caption Caption `json:"edge_media_to_caption"`
-		Owner   Owner   `json:"owner"`
-
-		IsVideo        bool   `json:"is_video"`
-		Title          string `json:"title"`
-		VideoURL       string `json:"video_url"`
-		VideoViewCount uint64 `json:"video_view_count"`
-
-		// clips_music_attribution_info
-		// media_overlay_info
-		// sharing_friction_info
-		// edge_media_to_sponsor_user
-		// is_affiliate
-		// is_paid_partnership
-		// location
-		// coauthor_producers
-
-	} `json:"shortcode_media"`
+	Media ShortcodeMedia `json:"shortcode_media"`
 }
 
 func (s EmbedResponse) IsEmpty() bool {
-	return s.ShortcodeMedia.Id == ""
+	return s.Media.Id == ""
 }
